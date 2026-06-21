c101dc708fb6cf78daa40a2bce28e4d6ed3877a0
CMattia06 <mattiacavaliere8@gmail.com>
2026-06-21 19:21:27 +0200

    creazione model classi

--- src/model/Vetrina.java
+++ src/model/Vetrina.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Vetrina {
+import model.titoli.TitoliInt;
+
+import java.util.ArrayList;
+
+public class Vetrina extends ArrayList<TitoliInt> {
+    static Vetrina v ;
+    private Vetrina(){
+
+    }
+
+    public Vetrina getInstance(){
+        if(v==null)
+            v=new Vetrina();
+        return v;
+    }
+
+
 }
